366bc202ed79c878ead0515bdfac0a6faded3e18
Kinan Principe <kinanprincipe@outlook.com>
2026-04-27 02:49:33 -0400

    adicionados comentarios de documentacao

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 
+// abre conexao com o banco de dados SQLite3
 const db = require('better-sqlite3')('database.sqlite3');
 
 const app = express();
 const PORT = 8080;
 
+// prepara middlewares utilizados pela biblioteca ExpressJS
 app.use(express.static('public'));
 app.use(express.json());
 
+// end-point que responde solicitacoes http GET com todos os usuarios cadastrados no sistema
 app.get('/api/usuarios', (req, res) => {
     const usuarios = db.prepare('SELECT * FROM usuarios;');
     let rpt = [];
@@ -18,15 +21,19 @@ app.get('/api/usuarios', (req, res) => {
     res.end(JSON.stringify(rpt));
 });
 
+// end-point que cadastra novos usuarios atraves de solicitacoes http POST
 app.post('/api/cadastrar', (req, res) => {
     try {
+        // verifica se o banco de dados existe para cadastro, se nao, criar
         db.exec('CREATE TABLE IF NOT EXISTS usuarios(id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT) STRICT;');
 
+        // insere na tabela `usuarios` dados vindos da solicitacao POST
         const stmt = db.prepare('INSERT INTO usuarios(nome) values(?);');
         stmt.run(req.body.nome);
     } catch(err) {
         console.log(err);
 
+        // responde codigo e mensagem de erro em caso de excecao encontrada
         res.send({
             codigo: err.code,
             mensagem: err.message,
@@ -35,6 +42,7 @@ app.post('/api/cadastrar', (req, res) => {
     res.end('sucesso!');
 });
 
+// abre servidor para escutar requisicoes dos clientes
 app.listen(PORT, () => {
     console.log(`listening to port ${PORT}...`);
 });
