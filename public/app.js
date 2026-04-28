b117cc0140e1dca6149ef363b377cfd41f776e9c
Kinan Principe <kinanprincipe@outlook.com>
2026-04-27 20:26:30 -0400

    melhora na interface do frontend e refatoracao

--- public/app.js
+++ public/app.js
@@ -1,33 +1,52 @@
+// endpoint da api que devolve todos os usuarios registrados no banco de dados
 const URL = 'http://127.0.0.1:8080/api/usuarios';
 
+// cria um elemento entry e insere no conteudo da pagina
+const criarEntry = (str_titulo, str_corpo, div) => {
+    const node_entry = document.createElement('div');
+    const node_titulo = document.createElement('h1');
+    const node_corpo = document.createElement('p');
+
+    node_titulo.append(str_titulo);
+    node_corpo.append(str_corpo);
+
+    node_entry.appendChild(node_titulo);
+    node_entry.appendChild(node_corpo);
+
+    node_entry.setAttribute('class', 'entry');
+    div.appendChild(node_entry);
+};
+
+// solicita lista de usuarios do endpoint USUARIOS
 async function atualizarUsuarios(url) {
     try {
-        console.log(url);
-
         const res = await fetch(url);
 
+        const content = document.getElementById('content');
+        
+        // lida com erros vindos do servidor e apresenta mensagem para o usuario
         if (!res.ok) {
+            criarEntry('Erro interno!', `Ocorreu alguma exceção interna, contate o seu administrador. Código de erro: ${res.status}`, content);
+            throw new Error(`status: ${res.status}`);
+        }
+        
+        // resposta em json vinda como resposta servidor
+        const usuarios = await res.json();
+
+        // exibe mensagem em caso de banco de dados sem cadastros
+        if (usuarios == null) {
+            criarEntry('Nenhum discente cadastrado!', '', content);
             throw new Error(`status: ${res.status}`);
         }
 
-        const usuarios = await res.json();
-
-        const content = document.getElementById('content');
+        // itera todos os usuarios vindo do banco de dados e criar entries para cada um no conteudo da pagina
         for (const usuario of usuarios) {
-            const entry = document.createElement('div');
-            const nome = document.createElement('h1');
-
-            nome.append(usuario.nome);
-            entry.appendChild(nome);
-
-            entry.setAttribute('class', 'entry');
-            content.appendChild(entry);
-
-            console.log(usuario.nome);
+            criarEntry(usuario.nome, 'ipsem lorem', content);
         }
     } catch (err) {
         console.log(err.message);
     }
 }
 
+// atualiza imediatamente ao carregar a pagina
 atualizarUsuarios(URL);
--- src/server.js
+++ src/server.js
@@ -12,8 +12,7 @@ app.use(express.json());
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
     if ('OPTIONS' == req.method) {
         res.sendStatus(200);
     }
